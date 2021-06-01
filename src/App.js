86a9f4cd30f7111c16a76c6d72e637644e57982f
aprin418 <aprin418@gmail.com>
2021-06-01 10:49:48 -0400

    not done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Game from "./components/Game";
+import Board from "./components/Board";
+import Square from "./components/Square";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Board />
+    </div>
+  );
+}
+
+export default App;
